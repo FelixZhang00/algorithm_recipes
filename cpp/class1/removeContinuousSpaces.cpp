10917e9e25b5c543b79a2fabaa30a93d30275762
Jamffy <l836828946@gmail.com>
2017-10-03 21:48:21 +0800

    removeContinuousSpaces

---
+++ cpp/class1/removeContinuousSpaces.cpp
@@ -0,0 +1,20 @@
+//
+// Created by felix on 2017/10/3.
+//
+
+/**
+ * 用unique()去除连续重复空白
+ * 题目：给你一个字符串，要求原地把相邻的多个空格替换为一个
+ * 例如，输入"a  b",输出"a b"
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+
+    return 0;
+}
+
