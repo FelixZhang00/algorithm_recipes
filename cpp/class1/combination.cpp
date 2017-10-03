897544fda476850efbfafb448ac1016bbda7035d
Jamffy <l836828946@gmail.com>
2017-10-03 21:27:02 +0800

    permutation

--- cpp/class1/combination.cpp
+++ cpp/class1/combination.cpp
@@ -2,6 +2,11 @@
 // Created by felix on 2017/10/3.
 //
 
+/**
+ * 生成从N个元素中取出M个的所有组合
+ * 输出从7个不同元素中取出3个元素的所有组合
+ */
+
 #include <iostream>
 
 int main() {
--- cpp/class1/permutation.cpp
+++ cpp/class1/permutation.cpp
@@ -2,10 +2,29 @@
 // Created by felix on 2017/10/3.
 //
 
+/**
+ * 题目：生成N个不同元素的全排列
+ * 使用next_permutation，会修改迭代器
+ */
+
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    int elements[] = { 1, 2, 3, 4 };
+    const size_t N = sizeof(elements)/sizeof(elements[0]);
+    std::vector<int> vec(elements, elements + N);
+
+    int count = 0;
+    do
+    {
+        std::cout << ++count << ": ";
+        std::copy(vec.begin(), vec.end(),
+                  std::ostream_iterator<int>(std::cout, ", "));
+        std::cout << std::endl;
+    } while (next_permutation(vec.begin(), vec.end()));
 }
 
